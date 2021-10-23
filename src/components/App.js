4f0b75885cd5e325cb5ffeab7f27fe499b5f664a
Daniel Won <dan.j.won@gmail.com>
2021-10-23 11:59:04 -0500

    add function to make API request to Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID U3nACStF6TpjFdcFMgl8hrLDsNljV6duxHiX3DfS1KY',
+			},
+		});
 	}
 
 	render() {

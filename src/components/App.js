e982cab40e26cf8f24b490d6237a88418d5737ce
Daniel Won <dan.j.won@gmail.com>
2021-10-24 10:04:40 -0500

    convert onSearchSubmit to async/await and set state

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,23 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
+	state = { images: [] };
+	async onSearchSubmit(term) {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization: 'Client-ID U3nACStF6TpjFdcFMgl8hrLDsNljV6duxHiX3DfS1KY',
 			},
 		});
+		console.log(this);
+		this.setState({ images: response.data.results });
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				Found: {this.state.images.length} images
 			</div>
 		);
 	}

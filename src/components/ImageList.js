6c90ae84c3f646c3485788dfe30eb10f6e144fb3
Daniel Won <dan.j.won@gmail.com>
2021-10-24 15:25:12 -0500

    add map method to loop over images array

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,10 @@
 const ImageList = (props) => {
-	console.log(props.images);
-	return <div>ImageList</div>;
+	// console.log(props.images);
+	const images = props.images.map((image) => {
+		return <img src={image.urls.regular} alt={'car no. 1'} />;
+	});
+
+	return <div>{images}</div>;
 };
 
 export default ImageList;

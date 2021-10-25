58f7716669caeb9b6c1c50b59e276a09ed834948
Daniel Won <dan.j.won@gmail.com>
2021-10-25 06:31:20 -0500

    update json files

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'https://cors-anywhere.herokuapp.com/https://api.unsplash.com',
+	baseURL: 'https://api.unsplash.com',
 	headers: {
 		Authorization: 'Client-ID U3nACStF6TpjFdcFMgl8hrLDsNljV6duxHiX3DfS1KY',
 	},

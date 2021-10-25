ffcafa964e9a4f8fa8d2f9425c114a4b076404b6
Daniel Won <dan.j.won@gmail.com>
2021-10-25 07:29:51 -0500

    import ImageList.css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
+import './ImageList.css';
+
 const ImageList = (props) => {
 	console.log(props.images);
 	const images = props.images.map(({ id, urls, description }) => {
 		return <img key={id} src={urls.regular} alt={description} />;
 	});
 
-	return <div>{images}</div>;
+	return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;

561488c6b42828ba4819290c72c071d102aac1df
Daniel Won <dan.j.won@gmail.com>
2021-10-25 07:42:50 -0500

    update ImageCard and and refactor ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 	console.log(props.images);
-	const images = props.images.map(({ id, urls, description }) => {
-		return <img key={id} src={urls.regular} alt={description} />;
+	const images = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image} />;
 	});
 
 	return <div className="image-list">{images}</div>;

08e43d18fd88e75f5b8e8b3ce6bf56f206889b6b
Daniel Won <dan.j.won@gmail.com>
2021-10-25 20:53:41 -0500

    update to access and set image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.imageRef = React.createRef();
+	}
+
+	componentDidMount() {
+		this.imageRef.current.addEventListener('load', this.setSpans);
+	}
+
+	setSpans = () => {
+		console.log(this.imageRef.current.clientHeight);
+	};
+
 	render() {
 		const { description, urls } = this.props.image;
 		return (
 			<div>
-				<img alt={description} src={urls.regular} />
+				<img ref={this.imageRef} src={urls.regular} alt={description} />
 			</div>
 		);
 	}

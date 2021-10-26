a1a25172645b66111944ca90c03e39843fc865db
Daniel Won <dan.j.won@gmail.com>
2021-10-26 06:07:14 -0500

    style search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,19 +11,35 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div className="ui segment">
-				<form onSubmit={this.onFormSubmit} className="ui form">
-					<div className="field">
-						<label>Image Search</label>
-						<input
-							type="text"
-							value={this.state.term}
-							onChange={(e) => {
-								this.setState({ term: e.target.value });
-							}}
-						/>
+			<div className="ui placeholder segment">
+				<div className="ui two column stackable center aligned grid">
+					<div className="middle aligned row">
+						<div className="column">
+							<div className="ui icon header">
+								<i class="search icon"></i>Image Search
+							</div>
+							<div className="field">
+								<div className="ui search">
+									<form onSubmit={this.onFormSubmit} className="ui form">
+										<div className="field">
+											<div className="ui icon input">
+												<input
+													type="text"
+													placeholder="Search any photos..."
+													value={this.state.term}
+													onChange={(e) => {
+														this.setState({ term: e.target.value });
+													}}
+												/>
+												<i class="search icon"></i>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
 					</div>
-				</form>
+				</div>
 			</div>
 		);
 	}
